564d8fb5da1a2e666832d3eaad9d77727ccfe918
perfect090 <perfectman090@gmail.com>
2018-11-27 22:28:12 +0100

    GET request to get-all-red-flag-records

--- src/routes/record.js
+++ src/routes/record.js
@@ -7,5 +7,6 @@ import users from '../../user.json';
 import recordController from '../controllers/record';
 
 router.post('/red-flags', recordController.createRecord);
+router.get('/red-flags', recordController.getAllRecords);
 
 export default router;
