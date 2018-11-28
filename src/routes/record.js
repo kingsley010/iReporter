e1f399050ef763329a986457e6bb27345be902f7
perfect090 <perfectman090@gmail.com>
2018-11-28 23:03:37 +0100

    PATCH request for editing a specific red-flag record

--- src/routes/record.js
+++ src/routes/record.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/red-flags', recordController.createRecord);
 router.get('/red-flags', recordController.fetchAllRecords);
 router.get('/red-flags/:id', recordController.fetchOneRecord);
+router.patch('/red-flags/:id', recordController.editOneRecord);
 
 export default router;
