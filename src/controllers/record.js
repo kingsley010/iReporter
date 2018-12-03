8a8150a4c81efae59be41a5c9ca7df819d752ba6
perfect090 <perfectman090@gmail.com>
2018-12-03 23:37:08 +0100

    Modified route

--- src/controllers/record.js
+++ src/controllers/record.js
@@ -2,36 +2,46 @@ import incident from '../../incident.json';
 import users from '../../user.json';
 
 const createRecord = (req, res) => {
-	const rcd = {
-		username: req.body.username,
-		title: req.body.title
-	};
-	if (rcd) {
-		res.status(201).json({
-		data: [{
-			id: 1,
-			message: 'created red-flag record',
-			createdOn: '26/11/18',
-			createdBy: '2',
-			type: 'red-flag',
-			location: 'lat: -34.397, lng: 150.644',
-			status: 'under investigation',
-			images: '[image, image]',
-			images: '[image, image]',
-			record: rcd
-		}]
-	});
-	} else {
-		res.status(404).json({
-			message: 'No post submitted'
-		})
-	}
+
+  const redflag = incident.records.redflags;
+  const newId = redflag.length + 1;
+  const day = new Date();
+  const createdBy = req.body.createdBy;
+  const location = req.body.location;
+  const comment = req.body.comment;
+
+
+  const rcd = {
+    id: newId,
+    createdOn: day,
+    createdBy: req.body.createdBy,
+    type: req.body.type,
+    location: req.body.location,
+    status: req.body.status,
+    Images: [],
+    Videos: [],
+    comment: req.body.comment
+  }
+
+  redflag.push(rcd);
+  return res.status(200).send({
+    status: 200,
+    data: [{
+      id: newId,
+      message: "Created red-flag record"
+    }]
+  });
 };
 
 const fetchAllRecords = (req, res) => {
-	res.status(200).json({
-		data: incident.records
-	});
+	const redflag = incident.records.redflags;
+	if (incident.records.redflags) {
+		res.status(200).json({
+        data : [{
+        	redflag
+        }]
+   });
+  }
 }
 
 // Exporting controller
