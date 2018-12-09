eb8923340e8698cccc959d7dd0cb6653019d62dc
perfect090 <perfectman090@gmail.com>
2018-12-09 23:27:57 +0100

    chore(database): implement database - add postgresql database
    [Finishes #162531572]

---
+++ src/controllers/auth.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import pg from 'pg';
+import dotenv from 'dotenv';
+import bcrypt from 'bcryptjs'
+// Importing Database Details
+import pool from '../models/db';
+
+dotenv.config();
+
+
+
+
+
+
+
+
